d28326c4fcbfa791b37dbc6fca22945531914efa
Logan Saso <jewsofhazard@gmail.com>
2015-02-18 01:45:39 -0800

    I removed the switch screen from our timer
    that way it wont conflict after we have removed it from the original
    bot.

--- src/me/jewsofhazzard/pcmrbot/util/Timer.java
+++ src/me/jewsofhazzard/pcmrbot/util/Timer.java
@@ -40,10 +40,7 @@ public class Timer implements Runnable {
 		MyBotMain.getConnectedchannel(channel).setTimer(true);
 		MyBotMain.getConnectedchannel(channel).setVoteCall(false);
 
-		if (screenSwitch) {
-			MyBotMain.getConnectedchannel(channel).switchScreen();
-			return;
-		}
+		
 		MyBotMain.getConnectedchannel(channel).voteCounter();
 		screenSwitch = false;
 
