923f8038573b3f8a860cc3fae2f51583fb8c7d03
Logan Saso <jewsofhazard@gmail.com>
2015-02-21 12:28:49 -0800

    Moved spam and autoreply to own methods

--- src/me/jewsofhazzard/pcmrbot/IRCBot.java
+++ src/me/jewsofhazzard/pcmrbot/IRCBot.java
@@ -126,36 +126,8 @@ public class IRCBot extends PircBot {
 		try {
 			Date date = new Date();
 			chatPostSeen.put(sender, channel.substring(1) + "|" + date.toString());
-			logger.info(sender+" - "+channel + "|" + date.toString());
 			
-			if (!isMod(sender)) {
-				if (message.matches("[A-Z]{20,}")) {
-					new Timeouts(connectedChannel, sender, 1, TType.CAPS);
-				} else if (message
-						.matches("(([A-Za-z0-9_:/\\-@\\s.]+[\\s?\\.\\s?]?)+([\\s?\\.\\s?](c\\s?o\\s?m|n\\s?e\\s?t|o\\s?r\\s?g|c\\s?o|a\\s?u|u\\s?k|u\\s?s|m\\s?e|b\\s?z|i\\s?n\\s?t|e\\s?d\\s?u|g\\s?o\\s?v\\s?|m\\s?i\\s?l|a\\s?c)(\\s)?(/)?)+[A-Za-z0-9_:/\\-@\\s.]+)+")) {
-					new Timeouts(connectedChannel, sender, 1, TType.LINK);
-				} else if (message.matches("[\\W_]{" +numSymbols + ",}")) {
-					new Timeouts(connectedChannel, sender, 1, TType.SYMBOLS);
-				} else if (message.length() >= paragraphLength) {
-					new Timeouts(connectedChannel, sender, 1, TType.PARAGRAPH);
-				} //else if (message
-					//	.matches("(:\\(|:\\)|:/|:D|:o|:p|:z|;\\)|;p|<3|>\\(|B\\)|o_o|R\\)|4Head|ANELE|ArsonNoSexy|AsianGlow|AtGL|AthenaPMS|AtIvy|BabyRage|AtWW|BatChest|BCWarrior|BibleThump|BigBrother|BionicBunion|BlargNaut|BloodTrail|BORT|BrainSlug|BrokeBack|BuddhaBar|CougarHunt|DAESuppy|DansGame|DatSheffy|DBstyle|DendiFace|DogFace|EagleEye|EleGiggle|EvilFetus|FailFish|FPSMarksman|FrankerZ|FreakinStinkin|FUNgineer|FunRun|FuzzyOtterOO|GasJoker|GingerPower|GrammarKing|HassaanChop|HassanChop|HeyGuys|HotPokket|HumbleLife|ItsBoshyTime|Jebaited|KZowl|JKanStyle|JonCarnage|KAPOW|Kappa|Keepo|KevinTurtle|Kippa|Kreygasm|KZassault|KZcover|KZguerilla|KZhelghast|KZskull|Mau5|mcaT|MechaSupes|MrDestructoid|MrDestructoid|MVGame|NightBat|NinjaTroll|NoNoSpot|noScope|NotAtk|OMGScoots|OneHand|OpieOP|OptimizePrime|panicBasket|PanicVis|PazPazowitz|PeoplesChamp|PermaSmug|PicoMause|PipeHype|PJHarley|PJSalt|PMSTwin|PogChamp|Poooound|PRChase|PunchTrees|PuppeyFace|RaccAttack|RalpherZ|RedCoat|ResidentSleeper|RitzMitz|RuleFive|Shazam|shazamicon|ShazBotstix|ShazBotstix|ShibeZ|SMOrc|SMSkull|SoBayed|SoonerLater|SriHead|SSSsss|StoneLightning|StrawBeary|SuperVinlin|SwiftRage|TF2John|TheRinger|TheTarFu|TheThing|ThunBeast|TinyFace|TooSpicy|TriHard|TTours|UleetBackup|UncleNox|UnSane|Volcania|WholeWheat|WinWaker|WTRuck|WutFace|YouWHY|\\(mooning\\)|\\(poolparty\\)|\\(puke\\)|:'\\(|:tf:|aPliS|BaconEffect|BasedGod|BroBalt|bttvNice|ButterSauce|cabbag3|CandianRage|CHAccepted|CiGrip|ConcernDoge|D:|DatSauce|FapFapFap|FishMoley|ForeverAlone|FuckYea|GabeN|HailHelix|HerbPerve|Hhhehehe|HHydro|iAMbh|iamsocal|iDog|JessSaiyan|JuliAwesome|KaRappa|KKona|LLuda|M&Mjc|ManlyScreams|NaM|OhGod|OhGodchanZ|OhhhKee|OhMyGoodness|PancakeMix|PedoBear|PedoNam|PokerFace|PoleDoge|RageFace|RebeccaBlack|RollIt!|rStrike|SexPanda|She'llBeRight|ShoopDaWhoop|SourPls|SuchFraud|SwedSwag|TaxiBro|tEh|ToasTy|TopHam|TwaT|UrnCrown|VisLaud|WatChuSay|WhatAYolk|YetiZ|PraiseIt|\\s){"+numEmotes+",}")) {
-					//new Timeouts(connectedChannel, sender, 1, TType.EMOTE);
-				//}
-				ResultSet rs = Database
-						.executeQuery("SELECT * FROM " + Database.DATABASE
-								+ "." + connectedChannel.substring(1) + "Spam");
-				try {
-					while (rs.next()) {
-						if (message.matches("(" + rs.getString(1) + ")+")) {
-							new Timeouts(connectedChannel, sender, 1, TType.SPAM);
-						}
-					}
-				} catch (SQLException e) {
-					logger.log(Level.SEVERE, "An error occurred checking if "
-							+ sender + "'s message has bad words", e);
-				}
-			} 
+			 checkSpam(message, sender);
 			
 			if(message.startsWith("!lmgtfy ")) {
 				message=message.substring(message.indexOf(' '));
@@ -465,31 +437,8 @@ public class IRCBot extends PircBot {
 				raffle(message);
 				
 			}
-			
-			ResultSet rs;
-			rs = Database.executeQuery("SELECT * FROM " + Database.DATABASE + "." + connectedChannel.substring(1) + "AutoReplies");
-			try {
-				while(rs.next()){
-					String [] keyword = rs.getString("keyword").split(",");
-					StringBuilder matchMe = new StringBuilder();
-					
-					for(int i = 0; i < keyword.length-1; i++){
-						matchMe.append("([\\s]*" + keyword[i] + "[\\s]*)+");
-					}
-					if(message.matches(matchMe.toString())){
-						
-						sendMessage(connectedChannel, rs.getString("reply"));
-						
-					}
-					
-				}
-			} catch (SQLException e) {
-				
-				logger.log(Level.SEVERE, "An error occured while trying to access the database.", e);
-			}
-		} catch (Exception e) {
-			logger.log(Level.SEVERE, "An error occurred while executing onMessage()", e);
 		}
+			finally {autoReplyCheck(message);}
 	}
 
 	/**
@@ -868,6 +817,67 @@ public class IRCBot extends PircBot {
 		
 	}
 	
+	public void autoReplyCheck(String message){
+		
+		ResultSet rs;
+		rs = Database.executeQuery("SELECT * FROM " + Database.DATABASE + "." + connectedChannel.substring(1) + "AutoReplies");
+		try {
+			while(rs.next()){
+				String [] keyword = rs.getString("keyword").split(",");
+				StringBuilder matchMe = new StringBuilder();
+				
+				for(int i = 0; i < keyword.length-1; i++){
+					matchMe.append("([\\s]*" + keyword[i] + "[\\s]*)+");
+				}
+				if(message.matches(matchMe.toString())){
+					
+					sendMessage(connectedChannel, rs.getString("reply"));
+					
+				}
+				
+			}
+		} catch (SQLException e) {
+			
+			logger.log(Level.SEVERE, "An error occured while trying to access the database.", e);
+		}
+		catch (Exception e) {
+			logger.log(Level.SEVERE, "An error occurred while executing onMessage()", e);
+		}
+	
+	}
+	
+	public void checkSpam(String message, String sender){
+		
+		if (!isMod(sender)) {
+			if (message.matches("[A-Z]{20,}")) {
+				new Timeouts(connectedChannel, sender, 1, TType.CAPS);
+			} else if (message
+					.matches("(([A-Za-z0-9_:/\\-@\\s.]+[\\s?\\.\\s?]?)+([\\s?\\.\\s?](c\\s?o\\s?m|n\\s?e\\s?t|o\\s?r\\s?g|c\\s?o|a\\s?u|u\\s?k|u\\s?s|m\\s?e|b\\s?z|i\\s?n\\s?t|e\\s?d\\s?u|g\\s?o\\s?v\\s?|m\\s?i\\s?l|a\\s?c)(\\s)?(/)?)+[A-Za-z0-9_:/\\-@\\s.]+)+")) {
+				new Timeouts(connectedChannel, sender, 1, TType.LINK);
+			} else if (message.matches("[\\W_]{" +numSymbols + ",}")) {
+				new Timeouts(connectedChannel, sender, 1, TType.SYMBOLS);
+			} else if (message.length() >= paragraphLength) {
+				new Timeouts(connectedChannel, sender, 1, TType.PARAGRAPH);
+			} //else if (message
+				//	.matches("(:\\(|:\\)|:/|:D|:o|:p|:z|;\\)|;p|<3|>\\(|B\\)|o_o|R\\)|4Head|ANELE|ArsonNoSexy|AsianGlow|AtGL|AthenaPMS|AtIvy|BabyRage|AtWW|BatChest|BCWarrior|BibleThump|BigBrother|BionicBunion|BlargNaut|BloodTrail|BORT|BrainSlug|BrokeBack|BuddhaBar|CougarHunt|DAESuppy|DansGame|DatSheffy|DBstyle|DendiFace|DogFace|EagleEye|EleGiggle|EvilFetus|FailFish|FPSMarksman|FrankerZ|FreakinStinkin|FUNgineer|FunRun|FuzzyOtterOO|GasJoker|GingerPower|GrammarKing|HassaanChop|HassanChop|HeyGuys|HotPokket|HumbleLife|ItsBoshyTime|Jebaited|KZowl|JKanStyle|JonCarnage|KAPOW|Kappa|Keepo|KevinTurtle|Kippa|Kreygasm|KZassault|KZcover|KZguerilla|KZhelghast|KZskull|Mau5|mcaT|MechaSupes|MrDestructoid|MrDestructoid|MVGame|NightBat|NinjaTroll|NoNoSpot|noScope|NotAtk|OMGScoots|OneHand|OpieOP|OptimizePrime|panicBasket|PanicVis|PazPazowitz|PeoplesChamp|PermaSmug|PicoMause|PipeHype|PJHarley|PJSalt|PMSTwin|PogChamp|Poooound|PRChase|PunchTrees|PuppeyFace|RaccAttack|RalpherZ|RedCoat|ResidentSleeper|RitzMitz|RuleFive|Shazam|shazamicon|ShazBotstix|ShazBotstix|ShibeZ|SMOrc|SMSkull|SoBayed|SoonerLater|SriHead|SSSsss|StoneLightning|StrawBeary|SuperVinlin|SwiftRage|TF2John|TheRinger|TheTarFu|TheThing|ThunBeast|TinyFace|TooSpicy|TriHard|TTours|UleetBackup|UncleNox|UnSane|Volcania|WholeWheat|WinWaker|WTRuck|WutFace|YouWHY|\\(mooning\\)|\\(poolparty\\)|\\(puke\\)|:'\\(|:tf:|aPliS|BaconEffect|BasedGod|BroBalt|bttvNice|ButterSauce|cabbag3|CandianRage|CHAccepted|CiGrip|ConcernDoge|D:|DatSauce|FapFapFap|FishMoley|ForeverAlone|FuckYea|GabeN|HailHelix|HerbPerve|Hhhehehe|HHydro|iAMbh|iamsocal|iDog|JessSaiyan|JuliAwesome|KaRappa|KKona|LLuda|M&Mjc|ManlyScreams|NaM|OhGod|OhGodchanZ|OhhhKee|OhMyGoodness|PancakeMix|PedoBear|PedoNam|PokerFace|PoleDoge|RageFace|RebeccaBlack|RollIt!|rStrike|SexPanda|She'llBeRight|ShoopDaWhoop|SourPls|SuchFraud|SwedSwag|TaxiBro|tEh|ToasTy|TopHam|TwaT|UrnCrown|VisLaud|WatChuSay|WhatAYolk|YetiZ|PraiseIt|\\s){"+numEmotes+",}")) {
+				//new Timeouts(connectedChannel, sender, 1, TType.EMOTE);
+			//}
+			ResultSet rs = Database
+					.executeQuery("SELECT * FROM " + Database.DATABASE
+							+ "." + connectedChannel.substring(1) + "Spam");
+			try {
+				while (rs.next()) {
+					if (message.matches("(" + rs.getString(1) + ")+")) {
+						new Timeouts(connectedChannel, sender, 1, TType.SPAM);
+					}
+				}
+			} catch (SQLException e) {
+				logger.log(Level.SEVERE, "An error occurred checking if "
+						+ sender + "'s message has bad words", e);
+			}
+		}
+	
+	}
 	
 
 }
