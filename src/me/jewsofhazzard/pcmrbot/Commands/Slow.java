6c42010319bf397ed588ec6d8d061536af074099
James Wolff <james.wolff0@gmail.com>
2015-02-22 03:00:06 -0600

    Moved Slow from the SuperClass to the Interface

--- src/me/jewsofhazzard/pcmrbot/Commands/Slow.java
+++ src/me/jewsofhazzard/pcmrbot/Commands/Slow.java
@@ -1,38 +1,42 @@
 package me.jewsofhazzard.pcmrbot.Commands;
 
-public class Slow extends Command {
-	
-	public Slow (String... params){
-		
-		super(params);
-		command = "slow";
-		
+public class Slow implements Command {
+
+	private String[] parameters;
+
+	private String reply;
+
+	public Slow(String... params) {
+
+		parameters = params;
+
 	}
-	
+
 	private boolean isSlow = false;
-	
-	public String getReply(){
-	
-		if(parameters[0].equalsIgnoreCase("true")){	
-		
-			if(!isSlow){
-			
-			return "/slow";
-				
+
+	public String getReply() {
+		return reply;
+	}
+
+	@Override
+	public void execute() {
+		if (parameters[0].equalsIgnoreCase("true")) {
+
+			if (!isSlow) {
+
+				reply = "/slow";
+
+			} else {
+
+				reply = "/slowoff";
+
 			}
-			else{
-				
-			return "/slowoff";
-				
-			}
-	
-		}
-	
-		else{
-			
-			return "I am sorry but the bot is not moderator in your channel and cannot run slowmode.";
-			
+
+		} else {
+
+			reply = "I am sorry but the bot is not moderator in your channel and cannot run slowmode.";
+
 		}
 	}
-	
+
 }
