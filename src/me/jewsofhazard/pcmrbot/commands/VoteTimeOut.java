75dbc4953f9367768ea4535b1a2da658c117d1b7
James Wolff <james.wolff0@gmail.com>
2015-02-23 15:19:39 -0600

    Added checks to voteTimeOut Command
    If the user is trying to create a voteTO is makes sure they are a mod
    first. If a voteTO is happenning in the channel it keeps them from
    overwriting the current one.

--- src/me/jewsofhazard/pcmrbot/commands/VoteTimeOut.java
+++ src/me/jewsofhazard/pcmrbot/commands/VoteTimeOut.java
@@ -1,6 +1,7 @@
 package me.jewsofhazard.pcmrbot.commands;
 
 import me.jewsofhazard.pcmrbot.MyBotMain;
+import me.jewsofhazard.pcmrbot.database.Database;
 import me.jewsofhazard.pcmrbot.util.CommandLevel;
 
 import org.jibble.pircbot.User;
@@ -19,6 +20,11 @@ public class VoteTimeOut extends Command implements ICommand {
 
 	@Override
 	public String execute(String channel, String sender, String... parameters) {
+		
+		if(MyBotMain.getBot().getVoteTimeOut(channel) != null) {
+			return "There is already a vote to timeout a user happenning in this channel. If you were trying to vote just type \"!voteTO\"!";
+		}
+		
 		String kickee=parameters[0];
 		
 		boolean exists = false;
@@ -27,15 +33,18 @@ public class VoteTimeOut extends Command implements ICommand {
 				exists=true;
 			}
 		}
-		if(exists) {
+		
+		if(exists && Database.isMod(sender, channel.substring(1))) {
 			MyBotMain.getBot().addVoteTimeOut(channel, new me.jewsofhazard.pcmrbot.util.VoteTimeOut(channel, kickee));
 			return "The channel has begun a vote to timeout %toKick%. Type !voteTO to place your vote. To vote no just do not vote.".replace("%toKick%", kickee);
 		}
+		
 		boolean startVote = true;
 		if(getCommandText().equalsIgnoreCase(kickee.substring(1))) {
 			startVote = false;
 		}
-		if(startVote) {
+		
+		if(startVote && Database.isMod(sender, channel.substring(1))) {
 			return "%kickee% is not in the channel!".replace("%kickee%", kickee);
 		}
 		return MyBotMain.getBot().getVoteTimeOut(channel).addVote(sender);
