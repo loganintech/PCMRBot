524b86108fa8228f414e9115cd17e44e637b0413
Logan Saso <jewsofhazard@gmail.com>
2015-02-23 08:20:20 -0800

    Revert "Finally fixed Auto Replies, they should work forever now"
    This reverts commit 1070a111e9022d93e755f86f4120afcf5200ebdf.

--- src/me/jewsofhazard/pcmrbot/commands/Setup.java
+++ src/me/jewsofhazard/pcmrbot/commands/Setup.java
@@ -7,23 +7,23 @@ public class Setup extends Command implements ICommand {
 
 	@Override
 	public String execute(String channel, String sender, String... parameters) {
-		if(!parameters[0].equalsIgnoreCase("continue")) {
+		if(parameters.length == 1) {
 			MyBotMain.getBot().sendMessage(
 				channel,
 				"To begin with, we use a two-part system to define a few options. Let's begin with timing out a user.");
 			MyBotMain.getBot().sendMessage(
 				channel,
-				"Users are timed out for excessive caps, symbols, emotes, long messages, links, and blacklisted words (spam).");
+				"Users are timed out for excessive caps, symbols, emotes, long messages, links, blacklisted words (spam).");
 			MyBotMain.getBot().sendMessage(
 				channel,
-				"We would like you to configure the ammount of capital letters, symbols, and emotes (all of which default to 10), and paragraph length(defaults to 250 characters) allowed in a message.");
+				"We would like you to configure the ammount of capital letters, symbols, and emotes (all of which default to 10), and paragraphs(defaults to 250 characters) allowed in a message.");
 			MyBotMain.getBot().sendMessage(
 				channel,
 				"To change this, please run !changeOption <caps, symbols, emotes, paragraph>|<new value>. Note: If you make paragraph to short users may not be able to post proper sentences. Think of it like twitter messages.");
 			MyBotMain.getBot().sendMessage(
 				channel,
 				"Once you are finished type \"!setup continue\" in chat!");
-		} else {
+		} else if(parameters[0].equalsIgnoreCase("continue")) {
 			MyBotMain.getBot().sendMessage(
 				channel,
 				"Next we are going to configure the welcome message for when new users enter your channel. It has a default (which should have been said to you by now), but if you want to change it you can type !changewelcome <message>.");
