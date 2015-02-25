59dd83f3b74a17e04dce0d355faa565befbbbbec
Angablade <Angablade@gmail.com>
2015-02-25 03:10:36 -0500

    Fixed the broken JS because I R STOOPID
    Yeah, fixed it..

--- js/tabchat.js
+++ js/tabchat.js
@@ -1,16 +1,15 @@
 function loadtwitch(username){
 
+			document.getElementById('twitch').setAttribute("style","height:15px; transition: height 1s;");
+		document.getElementById('twitch').innerHTML='<img src="./media/loading.gif"  height="15" width="15" />Loading.....';
 	
-	document.getElementById('twitch').setAttribute("style","height:15px; transition: height 1s;");
-	document.getElementById('twitch').innerHTML='<img src="./media/loading.gif"  height="15" width="15" />Loading.....';
 	
-	setTimeout(1000);
-	
-	switch (username) {
+	setTimeout(function(){
+		switch (username) {
 		case "angablade":
 			document.getElementById('twitch').innerHTML='<ul id="navtabs" class="navtabs"><li class="selected"><button>Angablade</button></li><li><button onclick="javascript:loadtwitch(\'donald10101\');">Donald10101</button></li><li><button onclick="javascript:loadtwitch(\'j3wsofhazard\');">J3wsOfHazard</button></li><li><button onclick="javascript:loadtwitch(\'officialpcmasterrace\');">Official PC Master Race</button></li></ul><br /><br /><iframe id="twitch-player" src="http://www.twitch.tv/angablade/embed" frameborder="0" scrolling="no" height="378" width="620">We\'re sorry. Your browser doesn\'t support Iframes. Please update your browser to view the embedded content.</iframe>&nbsp;<iframe id="twitch-chat" frameborder="0" scrolling="yes" id="chat_embed" src="http://www.twitch.tv/angablade/chat" height="378" width="300">We\'re sorry. Your browser doesn\'t support Iframes. Please update your browser to view the embedded content.</iframe>';
 			break;
-		
+
 		case "donald10101":
 			document.getElementById('twitch').innerHTML='<ul id="navtabs" class="navtabs"><li><button onclick="javascript:loadtwitch(\'angablade\');">Angablade</button></li><li class="selected"><button>Donald10101</button></li><li><button onclick="javascript:loadtwitch(\'j3wsofhazard\');">J3wsOfHazard</button></li><li><button onclick="javascript:loadtwitch(\'officialpcmasterrace\');">Official PC Master Race</button></li></ul><br /><br /><iframe id="twitch-player" src="http://www.twitch.tv/donald10101/embed" frameborder="0" scrolling="no" height="378" width="620">We\'re sorry. Your browser doesn\'t support Iframes. Please update your browser to view the embedded content.</iframe>&nbsp;<iframe id="twitch-chat" frameborder="0" scrolling="yes" id="chat_embed" src="http://www.twitch.tv/donald10101/chat" height="378" width="300">We\'re sorry. Your browser doesn\'t support Iframes. Please update your browser to view the embedded content.</iframe>';
 			break;
@@ -26,11 +25,16 @@ function loadtwitch(username){
 		default:
 			document.getElementById('twitch').innerHTML='<ul id="navtabs" class="navtabs"><li><button onclick="javascript:loadtwitch(\'angablade\');">Angablade</button></li><li><button onclick="javascript:loadtwitch(\'donald10101\');">Donald10101</button></li><li><button onclick="javascript:loadtwitch(\'j3wsofhazard\');">J3wsOfHazard</button></li><li class="selected"><button>Official PC Master Race</button></li></ul><br /><br /><iframe id="twitch-player" src="http://www.twitch.tv/officialpcmasterrace/embed" frameborder="0" scrolling="no" height="378" width="620">We\'re sorry. Your browser doesn\'t support Iframes. Please update your browser to view the embedded content.</iframe>&nbsp;<iframe id="twitch-chat" frameborder="0" scrolling="yes" id="chat_embed" src="http://www.twitch.tv/officialpcmasterrace/chat" height="378" width="300">We\'re sorry. Your browser doesn\'t support Iframes. Please update your browser to view the embedded content.</iframe>';
 			break;
-	}
+		}
+	},1000);
 	
-	document.getElementById('twitch').setAttribute("style","height:460px; transition: height 1s;");
 
-	setTimeout(1000);
+	
+	
+
+	setTimeout(function(){
+		document.getElementById('twitch').setAttribute("style","height:460px; transition: height 1s;");
+	},1000);
 
 }
 //<ul id="navtabs" class="navtabs"><li><button onclick="javascript:loadtwitch('angablade');">Angablade</button></li><li><button onclick="javascript:loadtwitch('donald10101');">Donald10101</button></li><li><button onclick="javascript:loadtwitch('j3wsofhazard');">J3wsOfHazard</button></li><li class="selected"><button onclick="javascript:loadtwitch('officialpcmasterrace');">Official PC Master Race</button></li></ul>
