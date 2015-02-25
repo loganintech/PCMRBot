b4bbd4c9276df25765cc5c89769787d775278c1e
Logan Saso <jewsofhazard@gmail.com>
2015-02-25 15:24:06 -0800

    Removed Redundancy in my brand new command. :/

---
+++ src/me/jewsofhazard/pcmrbot/commands/RNG.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.jewsofhazard.pcmrbot.commands;
+
+import java.util.Random;
+import me.jewsofhazard.pcmrbot.util.CLevel;
+
+public class RNG extends Command {
+    
+    private boolean inBounds = false;
+    
+    @Override
+    public CLevel getCommandLevel(){
+        
+        return CLevel.Normal;
+        
+    }
+    
+    @Override
+    public String getCommandText(){
+        
+        return "rng";
+        
+    }
+    
+    @Override
+    public String execute(String channel, String sender, String... parameters){
+    
+        Random rand = new Random();
+        return "Your random number is " + Integer.toString(rand.nextInt(Integer.valueOf(parameters[0] + 1))) + ".";
+        
+    }
+    
+}
