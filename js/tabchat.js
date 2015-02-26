7c36624793256912ef9c0d646ea7e933ac1e39a9
Angablade <Angablade@gmail.com>
2015-02-26 03:09:40 -0500

    Bla...
    It works.. Because I can.

--- js/tabchat.js
+++ js/tabchat.js
@@ -1,7 +1,9 @@
 function loadtwitch(username){
 
-	document.getElementById('twitch').innerHTML='<img src="./media/loading.gif"  height="15" width="15" />Loading.....';
-	
+	document.getElementById('twitch').innerHTML='<p id="vs" class="vs"><img src="./media/loading.gif"  height="15" width="15" />Loading.....</p>';
+		
+	document.getElementById('twitch').setAttribute("style", "background-color: black;");
+		
 	setTimeout(function(){
 		switch (username) {
 		case "angablade":
@@ -24,6 +26,11 @@ function loadtwitch(username){
 			document.getElementById('twitch').innerHTML='<ul id="navtabs" class="navtabs"><li><button onclick="javascript:loadtwitch(\'angablade\');">Angablade</button></li><li><button onclick="javascript:loadtwitch(\'donald10101\');">Donald10101</button></li><li><button onclick="javascript:loadtwitch(\'j3wsofhazard\');">J3wsOfHazard</button></li><li class="selected"><button>Official PC Master Race</button></li></ul><br /><br /><iframe id="twitch-player" src="http://www.twitch.tv/officialpcmasterrace/embed" frameborder="0" scrolling="no" height="378" width="620">We\'re sorry. Your browser doesn\'t support Iframes. Please update your browser to view the embedded content.</iframe>&nbsp;<iframe id="twitch-chat" frameborder="0" scrolling="yes" id="chat_embed" src="http://www.twitch.tv/officialpcmasterrace/chat" height="378" width="300">We\'re sorry. Your browser doesn\'t support Iframes. Please update your browser to view the embedded content.</iframe>';
 			break;
 		}
-	},1000);
+	},3000);
+	
+	setTimeout(function(){
+		document.getElementById('twitch').setAttribute("style", "background-color: white;");
+	},3000);
+	
 }
 //<ul id="navtabs" class="navtabs"><li><button onclick="javascript:loadtwitch('angablade');">Angablade</button></li><li><button onclick="javascript:loadtwitch('donald10101');">Donald10101</button></li><li><button onclick="javascript:loadtwitch('j3wsofhazard');">J3wsOfHazard</button></li><li class="selected"><button onclick="javascript:loadtwitch('officialpcmasterrace');">Official PC Master Race</button></li></ul>
