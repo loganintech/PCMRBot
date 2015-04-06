7f80908f2f5cdff671542888f55f98e30804dda6
Logan Saso <jewsofhazard@gmail.com>
2015-04-05 22:04:51 -0700

    Added command to see if whoever runs the command is a mod.

---
+++ src/me/jewsofhazard/pcmrbot/commands/AmIMod.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.jewsofhazard.pcmrbot.commands;
+
+import me.jewsofhazard.pcmrbot.database.Database;
+import me.jewsofhazard.pcmrbot.util.CLevel;
+
+/**
+ *
+ * @author Hazard
+ */
+public class AmIMod extends Command {
+    
+    @Override
+	public CLevel getCommandLevel() {
+		return CLevel.Mod;
+	}
+
+	@Override
+	public String getCommandText() {
+		return "amimod";
+	}
+
+	@Override
+	public String execute(String channel, String sender, String... parameters) {	
+            return "Do not worry " + sender + " you are a moderator in this channel.";
+	}
+    
+}
