79fba857d3e96ee617cb65b1374e46b768eb06ce
Logan Saso <jewsofhazard@gmail.com>
2015-04-12 15:56:38 -0700

    Made lolrank return level if not ranked, squashed bugs

--- src/me/jewsofhazard/pcmrbot/league/LeagueUtils.java
+++ src/me/jewsofhazard/pcmrbot/league/LeagueUtils.java
@@ -18,111 +18,98 @@ import com.robrua.orianna.type.core.summoner.Summoner;
  */
 public class LeagueUtils {
     private String region;
-    private String summoner;
+    private static Summoner summoner;
     
     private static String apiKey = "df57fbca-8417-4af6-92d0-3150cb01e1f7";    
     //private final String NAME_URL = "https://" + region + ".api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+summoner+"?api_key="+apiKey;
     
-    public LeagueUtils(String region, String summoner) throws Exception{
-    
-    this.region = region;
-    this.summoner = summoner;
-
-    }
     
     public static String getSummonerRank(String region, String summonerName){
-
-        if(region.toLowerCase().equals("na")){
-        RiotAPI.setMirror(Region.NA);
-        RiotAPI.setRegion(Region.NA);
+        String regionTest = setRegion(region);
         RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
+        summoner = RiotAPI.getSummonerByName(summonerName);
+        try{
+            if(regionTest.equals("-1")){
+                return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
+            }
         }
-        if(region.toLowerCase().equals("oce")){
-        RiotAPI.setMirror(Region.OCE);
-        RiotAPI.setRegion(Region.OCE);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
+        catch(Exception e){
+            return getLevel(region, summonerName);
         }
-        else if(region.toLowerCase().equals("pbe")){
-        RiotAPI.setMirror(Region.PBE);
-        RiotAPI.setRegion(Region.PBE);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
+        if(!regionTest.equals("-1")){
+        return regionTest;
         }
-        else if(region.toLowerCase().equals("ru")){
-        RiotAPI.setMirror(Region.RU);
-        RiotAPI.setRegion(Region.RU);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
+        return "";
+    }
+    
+    public static String setRegion(String region){
+    try{
+        switch (region.toLowerCase()) {
+            case "oce":
+                RiotAPI.setMirror(Region.OCE);
+                RiotAPI.setRegion(Region.OCE);
+                return "-1";
+            case "na":
+                RiotAPI.setMirror(Region.NA);
+                RiotAPI.setRegion(Region.NA);
+                return "-1";
+            case "pbe":
+                RiotAPI.setMirror(Region.PBE);
+                RiotAPI.setRegion(Region.PBE);
+                return "-1";
+            case "ru":
+                RiotAPI.setMirror(Region.RU);
+                RiotAPI.setRegion(Region.RU);
+                return "-1";
+            case "tr":
+                RiotAPI.setMirror(Region.TR);
+                RiotAPI.setRegion(Region.TR);
+                return "-1";
+            case "las":
+                RiotAPI.setMirror(Region.LAS);
+                RiotAPI.setRegion(Region.LAS);
+                return "-1";
+            case "lan":
+                RiotAPI.setMirror(Region.LAN);
+                RiotAPI.setRegion(Region.LAN);
+                return "-1";
+            case "kr":
+                RiotAPI.setMirror(Region.KR);
+                RiotAPI.setRegion(Region.KR);
+                return "-1";
+            case "euw":
+                RiotAPI.setMirror(Region.EUW);
+                RiotAPI.setRegion(Region.EUW);
+                return "-1";
+            case "eune":
+                RiotAPI.setMirror(Region.EUNE);
+                RiotAPI.setRegion(Region.EUNE);
+                return "-1";
+            case "br":
+                RiotAPI.setMirror(Region.BR);
+                RiotAPI.setRegion(Region.BR);
+                return "-1";
         }
-        else if(region.toLowerCase().equals("tr")){
-        RiotAPI.setMirror(Region.TR);
-        RiotAPI.setRegion(Region.TR);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
-        }
-        else if(region.toLowerCase().equals("las")){
-        RiotAPI.setMirror(Region.LAS);
-        RiotAPI.setRegion(Region.LAS);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
-        }
-        else if(region.toLowerCase().equals("lan")){
-        RiotAPI.setMirror(Region.LAN);
-        RiotAPI.setRegion(Region.LAN);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
-        }
-        else if(region.toLowerCase().equals("kr")){
-        RiotAPI.setMirror(Region.KR);
-        RiotAPI.setRegion(Region.KR);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
-        }
-        else if(region.toLowerCase().equals("euw")){
-        RiotAPI.setMirror(Region.EUW);
-        RiotAPI.setRegion(Region.EUW);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
-        }
-        else if(region.toLowerCase().equals("eune")){
-        RiotAPI.setMirror(Region.EUNE);
-        RiotAPI.setRegion(Region.EUNE);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
-        }
-        else if(region.toLowerCase().equals("br")){
-        RiotAPI.setMirror(Region.BR);
-        RiotAPI.setRegion(Region.BR);
-        RiotAPI.setAPIKey(apiKey);
-
-        Summoner summoner = RiotAPI.getSummonerByName(summonerName);
-        return summoner.getName() + " is in " + summoner.getLeagues().get(0).getTier().toString().toLowerCase();
-        }
-    return "You have used an incorrect region or summoner name, please try again.";
+    return "You have used an incorrect region, please try again.";
     }
    
+    catch(Exception e){
+    
+    return "You have used an incorrect region, please try again.";
+
+        }
     
     
+    }
+    
+    public static String getLevel(String region, String summonerName){  //this is broken, always catches
+    try{
+        return summoner.getName() + " is level " + summoner.getLevel();
+        }
+    catch(Exception e){
+        System.out.println(e);
+        //return "An error has occured checking for the level of the user, perhaps you used the wrong name or region?";
+        return "";
+        }
+    }
 }
